e6f3b1b71f88ec2294486ac4707d0d595d2e755b
Divya Tiwari <divyatiwari1112@gmail.com>
2021-06-16 16:52:05 +0530

    tree search working for static obstacles

--- TreeConstructionandReward.py
+++ TreeConstructionandReward.py
@@ -21,22 +21,22 @@ def add_node(parent, child):
 
 def reward_func(child,obs_dist_lat,obs_dist_long,prev_action,prev_reward,scene_len):
     reward = 0
-    k = 1
-    if child.pos[0] < scene_len and child.pos[0] > scene_len-1 and abs(child.pos[1]) < 0.9:
+    k = 10
+    if child.pos[0] < scene_len and child.pos[0] > scene_len-0.1 and abs(child.pos[1]) < 0.9375 :
         return 900
     if child.pos[0] >= scene_len or abs(child.pos[1]) >= 1.875:
         return -inf 
-    if child.time > scene_len/6.33:
+    if child.time >= scene_len/6.33:
         return -inf
-    # for i in range(len(obs_dist_long)):
-    #     if obs_dist_long[i][0] < child.pos[0] and obs_dist_long[i][1] > child.pos[0] and obs_dist_lat[i][0] < child.pos[1] and obs_dist_lat[i][1] > child.pos[1]:
-    #           return -inf
-    #     else:
-    #         # TODO reward for distance from obstacles 
-    #         pass
+    for i in range(len(obs_dist_long)):
+        if obs_dist_long[i][0] < child.pos[0] and obs_dist_long[i][1] > child.pos[0] and obs_dist_lat[i][0] < child.pos[1] and obs_dist_lat[i][1] > child.pos[1]:
+              return -inf
+        else:
+            reward = reward+1
 
-    reward = reward + k/(1 + exp(abs(20 - child.pos[0])))
-    reward = reward - k*(child.time)
+    # reward = reward + 100/(1 + exp(abs(scene_len - child.pos[0])))
+    reward = reward + k*1/(scene_len - child.pos[0])
+    reward = reward + k*1/((scene_len/6.33) - child.time)
     return reward
 
 
@@ -48,8 +48,7 @@ def check_validity(node):
 
 def generate_nodes(parent,obs_dist_lat,obs_dist_long,scene_len):
     #0->go left lane, 1-> stay, 2->go right
-    if parent.pos[0] > scene_len and abs(parent.pos[1]) >= 1.875:
-        return parent
+
 
     child = state(0,30,0,0,0,0,0,0,0)
     child1 = state(0,30,0,0,0,0,0,0,0)
@@ -74,7 +73,8 @@ def generate_nodes(parent,obs_dist_lat,obs_dist_long,scene_len):
     child.pos[1] = parent.pos[1] - 0.9375
     child.action = 0
     child.reward = reward_func(child,obs_dist_lat, obs_dist_long,parent.action,parent.reward,scene_len)
-    child.time =  parent.time + (sqrt(0.935**2 + (0.935*sqrt(3))**2)/6.33)
+    # child.time =  parent.time + (sqrt(0.935**2 + (0.935*sqrt(3))**2)/6.33)
+    child.time = parent.time + (1.875/6.33)
     # push in children of the parent 
     
     # call generate node on this generated node and pass parent as, parent.children[length(children)-1]
@@ -85,13 +85,11 @@ def generate_nodes(parent,obs_dist_lat,obs_dist_long,scene_len):
 
 
     child2.pos[0] = parent.pos[0] + 0.9375*(sqrt(3))
-    child2.pos[1] = parent.pos[1] + (0.9375)
+    child2.pos[1] = parent.pos[1] + 0.9375
     child2.action = 2
     child2.reward = reward_func(child2,obs_dist_lat, obs_dist_long,parent.action,parent.reward,scene_len)
-    child2.time =  parent.time + (sqrt(0.935**2 + (0.935*sqrt(3))**2)/6.33)
-    # push in children of the parent 
-    # if(child2.time > (10/6.33)):
-    #     print(child1.time)
+    # child2.time =  parent.time + (sqrt(0.935**2 + (0.935*sqrt(3))**2)/6.33)
+    child.time = parent.time + (1.875/6.33)
     # call generate node on this generated node and pass parent as, parent.children[length(children)-1]
     if check_validity(child2):
         # print(child2.pos[0])
@@ -135,21 +133,30 @@ def BFS(parent):
      
 if __name__ == "__main__":
     temp = state(0,30,0,0,0,0,0,0,0)
-    obs_lat = [[-1.975,-2.475]]
-    obs_long = [[10,10.5]]
-    scene_len = [20,22,24,25,26,30]
-    for count in scene_len:
-        t= time.time()
-        temp = generate_nodes(temp, obs_lat, obs_long,count)
-        print("Time taken to generate tree", time.time() - t)
-        ans = []
-        ans1 = [[]]
-        t1 = time.time()
-        dt,_ = DFS(temp,ans,ans1)
-        print("Time taken to traverse tree", time.time() - t1)
-        # break
-    # for i in dt:
-    #     # print(len(i))
-    #     for j in i:
-    #         print(j.pos[0], j.pos[1], j.reward)  
-    #     print("\n")
+    obs_lat = [[-1.875,-0.9375],[-0.9375,0.9375]]
+    obs_long = [[10,12.5],[14,16]]
+    scene_len = [20]
+    # for count in scene_len:
+    t= time.time()
+    temp = generate_nodes(temp, obs_lat, obs_long,20)
+    print("Time taken to generate tree", time.time() - t)
+    ans = []
+    ans1 = [[]]
+    t1 = time.time()
+    dt,_ = DFS(temp,ans,ans1)
+    print("Time taken to traverse tree", time.time() - t1)
+    prev_sum = -inf
+    sum = -inf
+    best_branch = []
+    reward_dict = {}
+    for i in dt:
+        for k in i:
+            sum = sum + k.reward
+        if sum > prev_sum:
+            print(sum,"\n")
+            print([[i[k].pos[0],i[k].pos[1]] for k in range(len(i))],"\n")
+            print([[i[k-1].action,i[k].pos[1]] for k in range(len(i))],"\n")
+            # please remember that you've mapped actions incorrectly
+            prev_sum = sum
+            best_branch = i
+        sum = 0
