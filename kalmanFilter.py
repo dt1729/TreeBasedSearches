50b0420f861deb708b122014537202c615e7c553
Divya Tiwari <divyatiwari1112@gmail.com>
2021-07-08 14:24:34 +0530

    Kalman filter complete

--- kalmanFilter.py
+++ kalmanFilter.py
@@ -1,13 +1,13 @@
 import numpy as np
 from randomNums import random_nums
-def Predict(prevState, prevCov, stateMatrix, controlMatrix, observationCov, controlValues):
+def Predict(prevState, prevCov, stateMatrix, controlMatrix, ProcessCov, controlValues):
     stateEstimate = stateMatrix.dot(prevState) + controlMatrix.dot(controlValues)
-    covEstimate = stateMatrix.dot(prevCov.dot(stateMatrix.T)) + observationCov
+    covEstimate = stateMatrix.dot(prevCov.dot(stateMatrix.T)) + ProcessCov
     return stateEstimate, covEstimate
 
-def KalmanGain(prevCov, stateObservation, statePredict, covPredict, observationMatrix):
+def KalmanGain(prevCov, stateObservation, statePredict, covPredict, observationMatrix,ObservationCov):
     measurementRes = stateObservation - observationMatrix.dot(statePredict)
-    covRes = observationMatrix.dot(covPredict.dot(observationMatrix.T)) # add R_k term
+    covRes = observationMatrix.dot(covPredict.dot(observationMatrix.T)) + ObservationCov# add R_k term
     # print(prevCov, "\n", covRes)
     KalGain = prevCov.dot((observationMatrix.T).dot(np.linalg.inv(covRes)))
     return KalGain, measurementRes
@@ -17,10 +17,10 @@ def Update(KalGain, statePredict, measurementRes, observationMatrix, covPredict)
     covUpdate  = (np.eye(observationMatrix.ndim)  - KalGain.dot(observationMatrix)).dot(covPredict)
     return stateUpdate, covUpdate
 
-def KalmanFilter(prevState, prevCov, controlValues, stateObservation, observationCov, stateMatrix, controlMatrix, observationMatrix):
-    statePredict, covPredict = Predict(prevState, prevCov, stateMatrix, controlMatrix, observationCov, controlValues) 
+def KalmanFilter(prevState, prevCov, controlValues, stateObservation, ProcessCov, stateMatrix, controlMatrix, observationMatrix):
+    statePredict, covPredict = Predict(prevState, prevCov, stateMatrix, controlMatrix, ProcessCov, controlValues) 
     # print("step1 done")
-    KalGain, measurementRes = KalmanGain(prevCov, stateObservation,statePredict, covPredict, observationMatrix)
+    KalGain, measurementRes = KalmanGain(prevCov, stateObservation,statePredict, covPredict, observationMatrix, ObservationCov)
     # print("step2 done")
     stateUpdate, covUpdate = Update(KalGain, statePredict, measurementRes, observationMatrix,covPredict)
     # print("step3 done")
@@ -29,9 +29,9 @@ def KalmanFilter(prevState, prevCov, controlValues, stateObservation, observatio
 
 
 if __name__ == "__main__":
-    x = np.array([0,0])
+    x = np.array([1,0])
     deltaT = 0.1
-    scov = np.eye(2)
+    scov = np.eye(2) # initial value of Process covariance matrix 
     scov[0][0] = 0.9 
     scov[0][1] = 0.1
     scov[1][0] = 0.2 
@@ -39,16 +39,17 @@ if __name__ == "__main__":
     stateMatrix = np.eye(2)
     controlMatrix = np.array([[deltaT,0],[0,deltaT]])
     observationMatrix = np.eye(2)
-    ObservationCov = np.array([[0.9,0.1],[0.1,0.9]])
+    ObservationCov = np.eye(2)
+    ProcessCov = np.array([[0.99,0.01],[0.09,0.91]]) # process covariance matrix value
     filteredStates = []
     filteredCov = []
-    # a = np.array([np.array([i/100 +random_nums[i][0],i/100 + random_nums[i][1]]) for i in range(1,100)])
-    a = np.array([np.array([i/100,i/100]) for i in range(1,100)])
+    a = np.array([np.array([i/10 +random_nums[i][0],i/100 + random_nums[i][1]]) for i in range(1,100)])
+    # a = np.array([np.array([i/100,i/100]) for i in range(1,100)])
     print(a, "\n")
-    controlValues = np.array([1,1])
+    controlValues = np.array([0.1,1])
     # todo controlValues, stateObservation, ObservationCov // Will be given inside a time loop
     for i in a:
-        x, scov = KalmanFilter(x,scov,controlValues,i,ObservationCov, stateMatrix, controlMatrix, observationMatrix)
+        x, scov = KalmanFilter(x,scov,controlValues,i,ProcessCov, stateMatrix, controlMatrix, observationMatrix)
         filteredStates.append(x)
         filteredCov.append(scov)
         print(x)
