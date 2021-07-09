b8c97e34f6b9936dbc3daee0215071c6903ade00
Divya Tiwari <divyatiwari1112@gmail.com>
2021-07-09 11:12:04 +0530

    added error plots

--- kalmanFilter.py
+++ kalmanFilter.py
@@ -9,7 +9,6 @@ def Predict(prevState, prevCov, stateMatrix, controlMatrix, ProcessCov, controlV
 def KalmanGain(prevCov, stateObservation, statePredict, covPredict, observationMatrix,ObservationCov):
     measurementRes = stateObservation - observationMatrix.dot(statePredict)
     covRes = observationMatrix.dot(covPredict.dot(observationMatrix.T)) + ObservationCov
-    # print(prevCov, "\n", covRes)
     KalGain = prevCov.dot((observationMatrix.T).dot(np.linalg.inv(covRes)))
     return KalGain, measurementRes
 
@@ -30,6 +29,7 @@ def KalmanFilter(prevState, prevCov, controlValues, stateObservation, ProcessCov
 
 
 if __name__ == "__main__":
+
     x = np.array([0,0])
     deltaT = 0.1
     scov = np.eye(2) # initial value of Process covariance matrix 
@@ -37,25 +37,32 @@ if __name__ == "__main__":
     scov[0][1] = 0.1
     scov[1][0] = 0.2 
     scov[1][1] = 0.8
-    stateMatrix = np.eye(2)
-    controlMatrix = np.array([[deltaT,0],[0,deltaT]])
-    observationMatrix = np.eye(2)
-    ObservationCov = np.array([[0.5,0.1],[0.2,0.8]]) # (R_k)
-    ProcessCov = np.array([[0.8,0.2],[0.09,0.91]]) # process covariance matrix value (Q_k)
+
+    stateMatrix = np.eye(2) # Ak
+    controlMatrix = np.array([[deltaT,0],[0,deltaT]]) #Bk
+    observationMatrix = np.eye(2) #Hk
+    ObservationCov = np.array([[0.6,0.4],[0,1]]) # (R_k)
+    ProcessCov = np.array([[0.7,0.3],[0.1,0.9]]) # process covariance matrix value (Q_k)
+
     filteredStates = []
     filteredCov = []
-    a = np.array([np.array([i/10 +random_nums[i][0],i**2/100 + random_nums[i][1]]) for i in range(1,100)])
-    actual = np.array([np.array([i/10,i**2/100]) for i in range(1,100)])
+
+    a = np.array([np.array([i/10 +random_nums[i][0],i/100 + random_nums[i][1]]) for i in range(1,100)]) # this line adds noise to the exact model 
+    actual = np.array([np.array([i/10,i/100]) for i in range(1,100)]) #exact model for plotting purposes
+
     print(a, "\n")
     prev = np.array([0,0])
     count = 0
+
     # todo controlValues, stateObservation, ObservationCov // Will be given inside a time loop
     for i in a:
-        controlValues = np.array([(actual[count][0] - prev[0])/deltaT, (actual[count][1] - prev[1])/deltaT])
+        # controlValues = np.array([(a[count][0] - prev[0])/deltaT, (a[count][1] - prev[1])/deltaT])
+        controlValues = np.array([1,0.1])
         x, scov = KalmanFilter(x,scov,controlValues,i,ProcessCov, stateMatrix, controlMatrix, observationMatrix)
         filteredStates.append(x)
         filteredCov.append(scov)
-        prev = i
+        # prev = i
+        prev = x
         count = count + 1
         print(x)
 
@@ -69,9 +76,22 @@ if __name__ == "__main__":
     pathTrackedx = [i[0] for i in actual]
     pathTrackedy = [i[1] for i in actual]
 
+    error_plotx = [a[i][0] - filteredStates[i][0] for i in range(len(a))]
+    error_ploty = [a[i][1] - filteredStates[i][1] for i in range(len(a))]
+
+    plt.figure()
+    plt.subplot(121)
     plt.plot(ansx,ansy,'b', label='Filtered')
     plt.plot(absx,absy,'r', label='Measured')
     plt.plot(pathTrackedx, pathTrackedy, 'g', label='Mathematical Model')
+    plt.title('Estimation')
+
+    plt.subplot(122)
+    plt.plot(error_plotx, label='Error in x')
+    plt.plot(error_ploty, label='Error in y')
+    plt.title('error')
+
+
     plt.legend()
     plt.show()
     
